ea4021caaca3862b8966b72f1234570b563a5d49
silviadmgz <silviadmgz@gmail.com>
2023-05-22 21:08:50 +0200

    Controllers added
    Player controller and camera are working!

--- Project Oguh 2.0/Assets/Scripts/CrashDetector.cs
+++ Project Oguh 2.0/Assets/Scripts/CrashDetector.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    bool hasCrashed;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other) 
     {
-        
+        if (other.tag == "MainCamera")
+        {
+            hasCrashed = true;
+        }
     }
 }
